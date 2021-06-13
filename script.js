2514548cef71b4ec52e9ad4efc8ec9d9f6ce4327
Motiur <82747429+motiurdev@users.noreply.github.com>
2021-06-14 00:44:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var progress = document.querySelector("#progress");
+var prev = document.querySelector("#pre");
+var next = document.querySelector("#next");
+var circles = document.querySelectorAll(".circle");
+
+var currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive++;
+    
+    if(currentActive > circles.length){
+        currentActive = circles.length;
+    }
+    update()
+})
+
+prev.addEventListener("click", () => {
+    currentActive--;
+    
+    if(currentActive < 1){
+        currentActive = 1;
+    }
+    update()
+})
+
+
+function update(){
+    circles.forEach( (circle,index) => {
+        if(index < currentActive){
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+    progress.style.width = (actives.length -1) / (circles.length-1)*100 + "%";
+
+    if(currentActive === 1){
+        prev.disabled = true;
+    }else if(currentActive == circles.length){
+        next.disabled = true;
+    }else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
